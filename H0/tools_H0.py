2ece383f21d095edf8a55cb003114d3c62fc2f1d
Giacomo Queirolo <astrogiacomoqueirolo@gmail.com>
2023-03-22 17:32:53 +0100

    minor H0
    better plot

--- H0/tools_H0.py
+++ H0/tools_H0.py
@@ -16,7 +16,49 @@ class H0_Res():
             self.sigma     = sigma 
     def __str__(self):
         return print_res_w_err(self.H0,self.sigma)+" "+str(self.unit)
+
+    def _get_other_H0(self,other):
+        if isinstance(other,H0_Res):
+            H0_prime=other.H0
+        elif isinstance(other,float) or isinstance(other,int):
+            H0_prime=other 
+        else:
+            raise TypeError(f"{other} not recognised, type must be H0_Res, float or int, not {type(other)}")
+        return H0_prime
+
+    def __eq__(self, other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0==H0_prime
     
+    def __ge__(self, other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0>=H0_prime
+    
+    def __gt__(self, other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0>H0_prime
+    
+    def __lt__(self, other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0<H0_prime
+    
+    def __le__(self, other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0<=H0_prime
+    
+    def __float__(self):
+        return self.H0
+
+    def __add__(self,other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0+H0_prime
+        
+    def __sub__(self,other):
+        H0_prime = self._get_other_H0(other)
+        return self.H0-H0_prime
+
+
+
 # planck  = 67.4 +- 0.5 km/s/Mpc
 h0planck = H0_Res(H0=67.4,sigma=0.5)
 # H0LiCoW = 73.3 -1.8+1.7  km/s/Mpc
--- Plots/plot_intermediate_H0_post_RW.py
+++ Plots/plot_intermediate_H0_post_RW.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[80]:
-
 
 """
 Plot to obtain intermediate H0 from single df of single filter model of lenstronomy and overplot the posterior
@@ -10,20 +8,16 @@ REWORKED
 """;
 
 
-# In[81]:
-
-
-import os,sys
+import sys
 import argparse
 import matplotlib
 import numpy as np
-import corner,pickle
-import json,copy,time
+import pickle
+import copy
 import matplotlib.pyplot as plt
-from scipy.stats import multivariate_normal
 
 from Utils.tools import *
-from TD_analysis import pycs_get_res 
+from TD_analysis import pycs_get_res
 from Utils.get_res import *
 from Utils.Dt_from_Df_reworked import *
 from H0.H0_Combined_reworked import *
@@ -34,7 +28,6 @@ from H0.tools_H0 import *
 from H0.H0_Combined_reworked import get_PH0
 
 
-# In[82]:
 
 
 font = {'family' : 'DejaVu Sans',
@@ -46,15 +39,13 @@ matplotlib.rc('figure',**{'figsize':(12,9)})
 plt.rc('xtick', labelsize=16)
 plt.rc('ytick', labelsize=16)
 
-
-# In[83]:
-
-
 if __name__=="__main__":
     ################################
     present_program(sys.argv[0])
     ################################
     parser = argparse.ArgumentParser(description="Temporary plot of the posterior of H0 from the given lens models")
+    parser.add_argument("-dtd","--dt_dataset",type=str, dest="dt_dataset", default="J1433_forcen",
+                    help="Dataset name for the time delay (default: J1433_forcen)")
     parser.add_argument("-nb","--number_bins",type=int, dest="nbins", default=100,
                     help="Number of bins per dimension for the histog. sampling of the Df (be careful with it! too many bins can be catastrophic)")
     parser.add_argument('SETTING_FILES',nargs="+",default=[],help="setting file(s) to consider")
@@ -62,12 +53,12 @@ if __name__=="__main__":
     # bins for the fermat pot
     bins  = args.nbins 
     setting_names =  args.SETTING_FILES
-
     
     #Dt:
-    print("Default time delay result obtained from: J1433_forcen")
+    dt_dataset=args.dt_dataset
+    print(f"Default time delay result obtained from: {dt_dataset}")
     pycs_path   = "./my_pycs_scripts/"
-    dt_comb_res = pycs_get_res.get_combined_res("J1433_forcen",main_dir_path=pycs_path)
+    dt_comb_res = pycs_get_res.get_combined_res(dt_dataset,main_dir_path=pycs_path)
     kwargs_dt   = get_kwdt(dt_comb_res)
     backup_res_lnstr = "./backup_results/"
     savefig_path     = backup_res_lnstr+"/Post_H0/"
@@ -75,7 +66,7 @@ if __name__=="__main__":
     
     H0s  = []
     PH0s = []
-    f,ax = plt.subplots(1,1,figsize=(12,12)) 
+    f,ax = plt.subplots(1,1,figsize=(12,12))     
     for i,sets in enumerate(setting_names):
         print("setting:",sets)
         mcmc_fermat = get_mcmc_fermat(sets)
@@ -94,11 +85,12 @@ if __name__=="__main__":
         yh0 = max(PH0)/2
         ax.errorbar(h0_res,yh0,yerr=None,xerr=[[err_min],[err_max]],fmt="r",capsize=4,c=base_colors[i])
 
+
     y_max = max([max(ph0) for ph0 in PH0s])
     ax.axvline(h0planck,label="Planck",c="r",ls="--")
     ax.axvline(h0licow,label="H0LiCOW",c="g",ls="--")
-    ax.fill_between(np.linspace(h0planck-h0planck_err ,h0planck+h0planck_err ) , -10, 10, color='red', alpha=0.2)
-    ax.fill_between(np.linspace(h0licow-h0licow_err[0] ,h0licow+h0licow_err[1] ) , -10, 10, color='green', alpha=0.2)
+    ax.fill_between(np.linspace(h0planck-h0planck.sigma_min,h0planck+h0planck.sigma_max ) , -10, 10, color='red', alpha=0.2)
+    ax.fill_between(np.linspace(h0licow-h0licow.sigma_min ,h0licow+h0licow.sigma_max) , -10, 10, color='green', alpha=0.2)
     plt.ylim(0,y_max*1.1)
     plt.xlabel("$H_0$[km/s/Mpc]")
     plt.ylabel("P($H_0$)")
